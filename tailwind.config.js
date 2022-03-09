6f7676df330a171879e94d4b254e92544c94645d
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-03-08 22:23:02 -0500

    [Config] Added purge configuration in Tailwind Config file to remove unwanted CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './dist/**/*.html',  // add an array of paths to all of your template files here
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
